f257c91795c4e8172985416a436ae673e1b755a5
Luca Fanciullini <luca.fanciullini@gmail.com>
2020-05-02 13:18:56 +0200

    Added thymeleaf poc

---
+++ SpringBootThymeleaf/src/main/java/it/arubapec/esecurity/springbootthymeleaf/controller/PageController.java
@@ -0,0 +1,16 @@
+package it.arubapec.esecurity.springbootthymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/international")
+    public String getInternationalPage(HttpSession session) {
+        return "international";
+    }
+
+}
