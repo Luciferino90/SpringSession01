1df33fc2a869fdac325d33380e88fc64ff8f9419
luca.fanciullini <luca.fanciullini@staff.aruba.it>
2020-05-29 18:55:54 +0200

    removed

--- maildemo/src/main/java/it/usuratonkachi/springsession/maildemo/config/KafkaEnabler.java
+++
@@ -1,9 +0,0 @@
-package it.usuratonkachi.springsession.maildemo.config;
-
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableBinding(KafkaChannels.class)
-public class KafkaEnabler {
-}
